ff3cb44854fc8d623a3d3dd00ead529eab62f581
Aparna Gurumurthy <47317631+AparnaGuru@users.noreply.github.com>
2022-07-20 12:44:38 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,13 +7,17 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/",(req,res) => {
         res.sendFile(__dirname+ "/views/index.html");
-        }
-)
+});
 
 app.get("/json",(req,res) => {
-        res.json({message: "Hello json"});
+        if(process.env.MESSAGE_STYLE=uppercase) {
+               res.json({message: "Hello World".toUpperCase()});
         }   
-)
+        else  {
+               res.json({message: "Hello World"});
+        }
+});
+
 
 
 
