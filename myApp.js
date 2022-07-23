acc245c16a9a596e570f8f56ca5e44774e8dcbe1
Aparna Gurumurthy <47317631+AparnaGuru@users.noreply.github.com>
2022-07-23 11:48:22 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,6 +5,10 @@ let app = express();
 
 console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
+app.use(req,res,next){
+        var string = req.method + " " + req.path + " - " + req.ip;
+        next();
+}
 
 app.get("/",(req,res) => {
         res.sendFile(__dirname+ "/views/index.html");
